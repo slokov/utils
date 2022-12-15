7169a5361ccee06bc358ce68c39d4cd618ed0953
Pavel Mironov <slk@users.noreply.github.com>
2022-12-15 22:58:34 +0300

    Change func name

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/slokov/utils/v2
+module github.com/slokov/utils/v3
 
 go 1.19
--- utils.go
+++ utils.go
@@ -11,7 +11,7 @@ func InSlice(a []string, x string) bool {
 }
 
 
-func ContainsInt(a []int, x int) bool {
+func InSliceInt(a []int, x int) bool {
   for _, n := range a {
      if x == n {
         return true
